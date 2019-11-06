7d65b3959a5c6cde80ad23b386a135b5f366cd1e
Derric Fragosa <fderric@gmail.com>
2019-11-06 22:03:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,13 @@ class Song
     song = self.new
     song.name = name 
     song
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
     
   end 
     
