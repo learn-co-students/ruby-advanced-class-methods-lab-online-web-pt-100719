3dbecf83f49c3e702c2581494c8c35b2c636bda7
Derric Fragosa <fderric@gmail.com>
2019-11-07 00:21:20 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -32,14 +32,14 @@ class Song
   end 
   
   def self.find_by_name(name)
-     if  @@all.detect { |song| song.name == name}
-    song
-     else 
-       false
-    end 
+      @@all.detect { |song| song.name == name}
+  end 
+  
+  def self.find_or_create_by_name
+      
   end 
 
-  
+
     
 #binding.pry 
 
